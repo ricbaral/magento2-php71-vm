822308312f41540f08ec90700169a587036dc1a9
Riccardo Baral <riccardo@codekunst.com>
2017-10-01 15:28:44 +0200

    Fix path to nginx configuration

--- puphpet/files/exec-always/post-provision.sh
+++ puphpet/files/exec-always/post-provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Copy nginx configuration..."
-cp /vagrant/nginx.conf.vm /etc/nginx/sites-available/_.conf
+cp /vagrant/nginx.conf /etc/nginx/sites-available/_.conf
 
 echo "Restart nginx..."
 service nginx restart
