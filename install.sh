aaab55bf665646f8309deb2c59240e131450bc87
Riccardo Baral <riccardo@codekunst.com>
2018-01-21 00:03:13 +0100

    Update base URL to magento2.loc

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 cd /var/www/shop
 
 echo "Installing Magento 2 via CLI..."
-bin/magento setup:install --base-url=http://magento2.dev/ --db-host=localhost --db-name=magento --db-user=vagrant --db-password=vagrant --admin-firstname=Magento --admin-lastname=User --admin-email=user@example.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD --timezone=Europe/Berlin --backend-frontname=admin --quiet
+bin/magento setup:install --base-url=http://magento2.loc/ --db-host=localhost --db-name=magento --db-user=vagrant --db-password=vagrant --admin-firstname=Magento --admin-lastname=User --admin-email=user@example.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD --timezone=Europe/Berlin --backend-frontname=admin --quiet
 
 echo "Setting developer mode..."
 bin/magento deploy:mode:set developer
@@ -15,8 +15,8 @@ echo "Disable cache: block_html and full_page..."
 bin/magento cache:disable block_html full_page
 
 echo
-echo "Frontend URL: http://magento2.dev/"
-echo "Backend URL: http://magento2.dev/admin"
+echo "Frontend URL: http://magento2.loc/"
+echo "Backend URL: http://magento2.loc/admin"
 echo "Admin User: admin"
 echo "Admin Password: admin123"
 echo
